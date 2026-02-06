1787eb1f23105f343647967799f2103d26760a55
Евгений Лактионов <laktionov_e@t.top-academy.ru>
2026-02-06 18:59:55 +0300

    Проверка буквы

--- Program.cs
+++ Program.cs
@@ -5,14 +5,18 @@
 создаем массив слов (предметы)
 
 выбираем случайное слово из массива (функция)
-выводим маску слова (функция)
+выводим слово 
 
-выводим слово
+создаем маску слова (функция)
+выводим маску
 
 не конец игры
 пока не конец игры
     запрашиваем у пользователя букву
     считываем букву
+    сверяем букву с маской (функция)
+    выводим маску
+
     конец игры (заглушка)
 
 выйти из программы
@@ -22,6 +26,13 @@
     для каждого символа в слове
         добавляем в маску символ '_'
     вернуть маску
+
+строка < обновить маску (слово, маска, буква)
+    создаем массив символов из маски
+    для каждого символа в слове
+        если символ совпадает с буквой
+            заменяем соответствующий символ в маске на букву
+    возвращаем обновленную маску
 */
 
 WriteTitle("Угадай слово!");
@@ -30,24 +41,27 @@ Random rnd = new Random();
 
 string[] words = { "стол", "книга", "окно", "лампа" };
 string secretWord = ChooseWord(words);
+// временно, для контроля разработки
+Console.WriteLine($"[DEBUG] Загаданное слово: {secretWord}");
 
 string maskedWord = CreateMask(secretWord);
 Console.WriteLine(maskedWord);
 
-// временно, для контроля разработки
-Console.WriteLine($"[DEBUG] Загаданное слово: {secretWord}");
-
 bool gameOver = false;
+int counter = 0;
 
 while(!gameOver)
 {
     Console.Write("Введи букву: ");
     char letter = Console.ReadLine()[0];
 
-    // временно завершаем цикл
-    gameOver = true;
-}
+    maskedWord = UpdateMask(secretWord, maskedWord, letter);
+    Console.WriteLine(maskedWord);
 
+    // временно завершаем цикл
+    if (counter++ == secretWord.Length)
+        gameOver = true;
+}
 
 ExitApp();
 
@@ -59,13 +73,25 @@ string ChooseWord(string[] words)
 
 string CreateMask(string word)
 {
-    //string mask = "";
     string mask = string.Empty;
 
     for(int i = 0; i < word.Length; i++)
         mask += "_";
 
     return mask;
+}
+
+string UpdateMask(string word, string mask, char letter)
+{
+    char[] result = mask.ToCharArray();
+
+    for(int i = 0; i < word.Length; i++)
+    {
+        if(word[i] == letter)
+            result[i] = letter;
+    }
+
+    return new string(result);
 }
 
 void WriteTitle(string title)
